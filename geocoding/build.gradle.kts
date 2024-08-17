21c70d2a6f74fbc19ee99dfda9a58ebb3989e021
Denis Orekhov <3008denis@gmail.com>
2024-08-17 10:43:54 +0300

    Create bean for objectMapper

--- geocoding/build.gradle.kts
+++ geocoding/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.3.3-SNAPSHOT"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("io.freefair.lombok") version "8.6"
+	application
+	checkstyle
+	jacoco
 }
 
 group = "com.geo"
@@ -19,6 +23,10 @@ configurations {
 	}
 }
 
+application {
+	mainClass = "com.geo.geocoding.GeocodingApplication"
+}
+
 repositories {
 	mavenCentral()
 	maven { url = uri("https://repo.spring.io/snapshot") }
@@ -33,8 +41,36 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	implementation("jakarta.validation:jakarta.validation-api:3.1.0")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+
+	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+
+	implementation("org.mapstruct:mapstruct:1.5.5.Final")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+
+	implementation("net.datafaker:datafaker:2.0.2")
+	implementation("org.instancio:instancio-junit:3.3.0")
+
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.6.0")
+	implementation("org.springdoc:springdoc-openapi-starter-webflux-api:2.6.0")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+	finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test)
+	reports {
+		xml.required = true
+	}
 }
--- geocoding/src/main/java/com/geo/geocoding/GeocodingApplication.java
+++ geocoding/src/main/java/com/geo/geocoding/GeocodingApplication.java
@@ -2,8 +2,10 @@ package com.geo.geocoding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GeocodingApplication {
 
 	public static void main(String[] args) {
